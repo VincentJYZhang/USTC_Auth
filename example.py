5cbd5205577409e53d7eb97e218d8ebbdafdf7ee
Junyang Zhang <vincentzjy@outlook.com>
2021-11-16 20:37:24 +0800

    Create example.py

---
+++ example.py
@@ -0,0 +1,23 @@
+"""
+这是个选学术报告的例子
+非常简便，仅作参考
+初代版本见：https://github.com/VincentJYZhang/USTC_Lecture/blob/main/lecture_select/lecture_select.py
+"""
+
+from ustc_auth import USTC_Auth
+from urllib import parse
+
+
+# 直接实例化，已经与服务器建立session
+auth = USTC_Auth('SA21******', '***********')  # 填写学号、密码
+
+
+url = 'http://yjs.ustc.edu.cn/bgzy/m_bgxk_up.asp'
+
+lecture_id = '1111111111111' # 学术报告的编号
+
+# 生成urlencode的post data格式
+form_data = parse.urlencode({"selectxh": int(lecture_id), "select": "true"})
+
+# 直接post
+auth.post_with_headers(url, data = form_data)
